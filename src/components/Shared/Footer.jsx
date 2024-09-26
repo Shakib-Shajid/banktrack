92938369287eb1c86d264c7cfac8415600a21f8d
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-09-27 01:56:33 +0600

    new footer added after a footer

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -10,11 +10,11 @@ import { FaYoutube } from "react-icons/fa";
 const Footer = () => {
     return (
         <div className='bg-[#FDFAF5]'>
-            <footer className="footer flex flex-col lg:grid lg:grid-cols-4 text-base-content p-10 w-[90%] mx-auto relative">
+            <footer className="footer flex flex-col lg:grid lg:grid-cols-4 text-base-content p-5 md:p-10 w-[90%] mx-auto relative">
                 <aside>
                     <Image src="/logo.png" width="70" height="70" alt="" className='absolute right-0 lg:relative' />
                 </aside>
-                
+
                 <nav className='space-y-2'>
                     <h6 className="footer-title text-[#888585]">Sobre Banktrack</h6>
                     <a className="link link-hover">Funcionalidades</a>
@@ -55,8 +55,37 @@ const Footer = () => {
                     </div>
                 </nav>
             </footer>
+            
+            <footer className="footer px-5 md:px-10 py-4 w-[90%] mx-auto gap-4 text-[#888585] flex flex-col text-start lg:grid">
+                <div className='lg:flex gap-2 items-baseline'>
+                    <form>
+                        <select className='border-2 text-black'>
+                            <option>Español</option>
+                            <option>English</option>
+                        </select>
+                    </form >
+                    <p>2024© Bank2Email - Todos los derechos reservados.</p>
+                </div>
+                <nav className="lg:place-self-center lg:justify-self-end">
+                    <div className="flex flex-col lg:grid lg:grid-flow-col gap-4">
+                        <a>
+                            Términos y condiciones
+                        </a>
+                        <a>
+                            Política de privacidad
+                        </a>
+                        <a>
+                            Gestionar cookies
+                        </a>
+                    </div>
+                </nav>
+            </footer>
         </div>
     );
 };
 
 export default Footer;
+
+
+
+
