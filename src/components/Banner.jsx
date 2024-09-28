e0ee12e18ed84c07b703e41dc6c8cf931d8652d1
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-09-28 22:35:39 +0600

    banner button design updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
                             Visualiza todos tus gastos e ingresos a tiempo real, categorizados y desde una única plataforma
                         </p>
                         <div className='flex justify-center gap-3 flex-col md:flex-row'>
-                            <Link href="/registrarse"><button className="btn bg-[#4021C8] text-white rounded-full btn-lg px-10"><div>Empieza gratis</div></button></Link>
+                            <Link href="/registrarse"><button className="btn bg-[#4021C8] text-white rounded-full btn-lg px-10 w-full"><div>Empieza gratis</div></button></Link>
                             <button className="btn rounded-full btn-lg px-10 bg-white text-[#4021C8]"><div className='flex gap-1 justify-center items-center'><CiCalendar className='text-[#4021C8] text-3xl w-8' />Agendar demo</div></button>
                         </div>
 
