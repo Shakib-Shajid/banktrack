8eeb284a0849639734bb3bd460f758e25f97b9d9
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-09-28 22:14:42 +0600

    registration links added in another apges

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { IoMdCheckmark } from 'react-icons/io';
 
@@ -15,7 +16,7 @@ const Data = () => {
                 <div className="hero-content text-center bg-white rounded-xl py-10 md:px-20 md:py-10 mx-10">
                     <div className="">
                         <h1 className="text-4xl font-extrabold mb-5">Pruébalo <br className='md:hidden' /> ahora con <br className='hidden md:grid' /> tus datos</h1>
-                        <button className="btn btn-primary rounded-full text-lg text-white btn-lg  px-10">Ver dashboard con mis datos</button>
+                        <Link href="/registrarse"><button className="btn btn-primary rounded-full text-lg text-white btn-lg  px-10">Ver dashboard con mis datos</button></Link>
                         <div className='flex flex-col md:flex-row gap-3 text-[#888585] my-5'>
                             <p><IoMdCheckmark className='inline-block' /> Tu cuenta gratis en 2 minutos</p>
                             <p><IoMdCheckmark className='inline-block' /> Sin tarjeta de crédito</p>
