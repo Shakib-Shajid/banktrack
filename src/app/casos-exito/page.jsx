341dbdd3f9ad450ad791e6a90c06d29041a38e8a
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-09-30 00:45:50 +0600

    client review page booking section added

--- src/app/casos-exito/page.jsx
+++ src/app/casos-exito/page.jsx
@@ -1,5 +1,7 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { CiCalendar } from 'react-icons/ci';
 import { RiDoubleQuotesL } from "react-icons/ri";
 
 const page = () => {
@@ -170,6 +172,26 @@ const page = () => {
                         <h2 className="card-title text-2xl">Autocares David</h2>
                         <p className='text-[#4021C8] text-base font-semibold'>Empresa de Movilidad</p>
                     </div>
+                </div>
+            </div>
+
+
+            <div className='bg-gradient-to-r from-violet-600 to-blue-500 h-full flex gap-5 md:w-[80%] mx-auto justify-between md:rounded-3xl my-10'>
+                <div className='py-10 w-[86%] lg:w-[50%] mx-auto my-auto'>
+                    <p className='font-bold'>Pruébalo</p>
+                    <h2 className='text-white text-3xl lg:text-5xl font-bold leading-tight'>Simplifica tus finanzas. Dedica más tiempo a tu negocio.</h2>
+                    <div className='my-7'>
+                        <div className='flex justify-start gap-3 flex-col md:flex-row '>
+                            <Link href="/registrarse">
+                                <button className="btn bg-gradient-to-r from-red-300 to-red-500 text-white rounded-full btn-lg px-10 w-full border-none"><div>Empieza gratis</div></button>
+                            </Link>
+
+                            <button className="btn rounded-full btn-lg px-10 bg-white text-[#4021C8] border-none"><div className='flex gap-1 justify-center items-center'><CiCalendar className='text-[#4021C8] text-3xl w-8' />Agendar demo</div></button>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-[30%] text-start h-full hidden lg:grid">
+                    <Image src="/client.png" width="500" height="500" alt="" className='hidden lg:grid w-full rounded-3xl' />
                 </div>
             </div>
         </div>
